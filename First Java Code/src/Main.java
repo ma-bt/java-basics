c15994fcdb3d53a49c870e9d42719268c2739153
Mandira Basnet <mandeerab20@gmail.com>
2024-01-26 00:51:19 +0545

    feat: mortgage calculation

--- First Java Code/src/Main.java
+++ First Java Code/src/Main.java
@@ -7,37 +7,32 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       final int MONTHS = 12;
-       final int PERCENT = 100;
+        final int MONTHS = 12;
+        final int PERCENT = 100;
         int PAYMENTS;
         int INTEREST_RATE;
-        float RATE;
-        float MORTGAGE;
+        float MONTHLY_RATE;
+        double MORTGAGE;
 
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Principal: ");
         int PRINCIPLE = scanner.nextInt();
 
         System.out.print("Annual Interest Rate: ");
-        float annual_interest_rate = scanner.nextInt();
+        float annual_interest_rate = scanner.nextFloat();
 
 
-        RATE = annual_interest_rate/MONTHS * PERCENT;
+        MONTHLY_RATE = (annual_interest_rate / PERCENT) / MONTHS;
+        System.out.println("Monthly rate: " + MONTHLY_RATE);
         System.out.print("Period in Years: ");
         int time_period = scanner.nextInt();
 
-        int TOTAL_NUMBER_OF_PAYMENTS = time_period/MONTHS;
+        int TOTAL_NUMBER_OF_PAYMENTS = time_period * MONTHS;
 
-
-
-
-
-
-
-
-
-
+        MORTGAGE = PRINCIPLE * (MONTHLY_RATE * Math.pow(1 + MONTHLY_RATE, TOTAL_NUMBER_OF_PAYMENTS)) / (Math.pow(1 + MONTHLY_RATE, TOTAL_NUMBER_OF_PAYMENTS) - 1);
+//        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(MORTGAGE);
+        System.out.println("Mortgage: " + MORTGAGE);
 
 
 //        Date now = new Date();
