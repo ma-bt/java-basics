e87778151928a8c97c78b6220c50c637af4c45e6
Mandira Basnet <mandeerab20@gmail.com>
2024-01-28 23:25:53 +0545

    code cleanup

--- First Java Code/src/Main.java
+++ First Java Code/src/Main.java
@@ -31,30 +31,10 @@ public class Main {
         int TOTAL_NUMBER_OF_PAYMENTS = time_period * MONTHS;
 
         MORTGAGE = PRINCIPLE * (MONTHLY_RATE * Math.pow(1 + MONTHLY_RATE, TOTAL_NUMBER_OF_PAYMENTS)) / (Math.pow(1 + MONTHLY_RATE, TOTAL_NUMBER_OF_PAYMENTS) - 1);
-//        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(MORTGAGE);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(MORTGAGE);
         System.out.println("Mortgage: " + MORTGAGE);
 
 
-//        Date now = new Date();
-//        String message = "Hello World    " + " !! ";
-//        System.out.println(message.trim());
-//        //explicit casting where developer changes the type of variable
-//        double x = 1.1;
-//        int y = (int) x + 2;
-//        System.out.println(y);
-//
-//
-//        String z = "5";
-//        int a = Integer.parseInt(z) + 2; //type changing of string into int
-//        System.out.println(a);
-//
-//        //Math function
-//        int result = (int) Math.floor(42.52);
-//        int randomNumber = (int) (Math.random() * 101);
-//        System.out.println(randomNumber);
-//
-//        //abstract class
-//        String abs = NumberFormat.getPercentInstance().format(0.1);
-//        System.out.println(abs);
+
     }
 }
