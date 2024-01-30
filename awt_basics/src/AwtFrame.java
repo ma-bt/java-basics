b232b3208317621d15ec9e374e7df7b69d816506
Mandira Basnet <mandeerab20@gmail.com>
2024-01-31 01:03:38 +0545

    feat: button addActionListener to change label

--- awt_basics/src/AwtFrame.java
+++ awt_basics/src/AwtFrame.java
@@ -2,25 +2,41 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class AwtFrame {
+
+
     public AwtFrame() {
         Frame frame = new Frame();
 
         Label label = new Label("This is an Label");
-        label.setBounds(40, 90, 150,30);
+        label.setBounds(40, 90, 150, 30);
 
         TextField textField = new TextField("John Disuja");
         textField.setBounds(40, 120, 150, 30);
 
-        Button button  = new Button("Click Me");
-        button.setBounds(40,50,80,40);
+        TextArea textArea = new TextArea();
+        textArea.setBounds(40, 200, 200, 200);
+
+        Checkbox checkbox = new Checkbox("Checkbox");
+        checkbox.setBounds(40, 150, 80, 40);
+
+        Button button = new Button("Click Me");
+        button.setBounds(40, 50, 80, 40);
         button.setBackground(Color.gray);
         button.setForeground(Color.white);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("Button click successfully");
+            }
+        });
 
         frame.setTitle("This is Frame title");
         frame.add(button);
         frame.add(label);
         frame.add(textField);
-        frame.setSize(400, 400);
+        frame.add(checkbox);
+        frame.add(textArea);
+        frame.setSize(700, 700);
         frame.setLayout(null);
         frame.setVisible(true);
 
@@ -32,12 +48,9 @@ public class AwtFrame {
         });// for frame closing button
 
     }
-    //    public AwtFrame()  {
-    //        setSize(400,300);
-    //        setVisible(true);
-    //    }
+
 
     public static void main(String[] args) {
-       AwtFrame frame =  new AwtFrame();
+        AwtFrame frame = new AwtFrame();
     }
 }
